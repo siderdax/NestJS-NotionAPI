464c1208e9b14e014545eb705b840761119a99a2
KYI <headwaving@gmail.com>
2022-07-24 22:35:05 +0900

    Move passport config from AppModule to AuthModule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,33 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { NotionModule } from './notion/notion.module';
+import { PassportSerializer } from '@nestjs/passport';
 
 @Module({
   controllers: [AuthController],
   providers: [AuthService],
   imports: [NotionModule],
 })
-export class AuthModule {}
+export class AuthModule extends PassportSerializer implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(
+        this.getPassportInstance().initialize(),
+        this.getPassportInstance().session(),
+      )
+      .forRoutes(AuthController);
+  }
+
+  // passport
+  serializeUser(user: any, done: any) {
+    process.nextTick(function () {
+      done(null, user);
+    });
+  }
+  deserializeUser(user: any, done: any) {
+    process.nextTick(function () {
+      return done(null, user);
+    });
+  }
+}
